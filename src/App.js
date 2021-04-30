19315e1833073877a010c8c406af3bfdef695025
Kelly <lawrencek1992@gmail.com>
2021-04-30 00:13:48 -0600

    Update Home.js styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 import Header from './components/Header.js';
 import Home from './components/Home.js';
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
 
 const Home = () => {
     return (
-        <Container>
-            <Col>
-                <Row>
-                    <Image className="main-image" src="https://i.ibb.co/njyy1r3/blue-bowl.png" alt="Bowl of water on a fence post in a field." />
-                </Row>
-                <Row>
-                    <p>This is the home page!</p>
-                </Row>
-            </Col>
+        <Container fluid className="home-container" bg="dark">
+            <Image className="main-image" src="https://i.ibb.co/njyy1r3/blue-bowl.png" alt="Bowl of water on a fence post in a field." fluid />
+            <p>This is the home page!</p>
         </Container>
     )
 }

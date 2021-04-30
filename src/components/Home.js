d0ece1470cac5a0e0317278f01619529e96ddb38
Kelly <lawrencek1992@gmail.com>
2021-04-30 11:00:28 -0600

    Add family photo to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,20 @@ import Image from 'react-bootstrap/Image';
 
 const Home = () => {
     return (
-        <Container fluid className="home-container" bg="dark">
-            <Image className="main-image" src="https://i.ibb.co/njyy1r3/blue-bowl.png" alt="Bowl of water on a fence post in a field." fluid />
+        <Container className="home-container" fluid >
+            <Image className="main-image" src="https://i.ibb.co/njyy1r3/blue-bowl.png" fluid />
             <h1 className="title">Beyond Nature CO</h1>
-            <p className="home-description">
-                My name is Kayla Eggemeyer, and I create gorgeos art pieces for patios, flower gardens, or even indoors! All of my designs are created from upcycled local Colorado materials. 
-                <br /> <br />
-                Check out the highlights section of my site to see some of my best work, and please visit my Instagram or Facebook pages to view the pieces I currently have in stock!
-            </p>
+            <div className="home-description">
+                <p>
+                    My name is Kayla Eggemeyer, and I create gorgeos art pieces for patios, flower gardens, or even indoors! All of my designs are created from upcycled local Colorado materials.
+                </p>
+                <p>
+                    Check out the highlights section of my site to see some of my best work, and please visit my Instagram or Facebook pages to view the pieces I currently have in stock!
+                </p>
+                <Image className="family-photo" src="https://i.ibb.co/TkmL8F5/family.png" alt="Kayla and her husband stand with their two children in front of a lake." fluid />
+            </div>
         </Container>
     )
-}
+};
 
 export default Home;

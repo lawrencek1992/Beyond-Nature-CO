55437cfb43c9963b375bed80f8be925e6f70f365
Kelly <lawrencek1992@gmail.com>
2021-04-30 09:56:56 -0600

    Add description to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,12 @@ const Home = () => {
     return (
         <Container fluid className="home-container" bg="dark">
             <Image className="main-image" src="https://i.ibb.co/njyy1r3/blue-bowl.png" alt="Bowl of water on a fence post in a field." fluid />
-            <p>This is the home page!</p>
+            <h1 className="title">Beyond Nature CO</h1>
+            <p className="home-description">
+                My name is Kayla Eggemeyer, and I create gorgeos art pieces for patios, flower gardens, or even indoors! All of my designs are created from upcycled local Colorado materials. 
+                <br /> <br />
+                Check out the highlights section of my site to see some of my best work, and please visit my Instagram or Facebook pages to view the pieces I currently have in stock!
+            </p>
         </Container>
     )
 }

b24f8569b71026500713939ea9efccdbce4b54a5
Kelly <lawrencek1992@gmail.com>
2021-04-29 22:28:58 -0600

    Update img

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <img src="https://i.ibb.co/T1NBtY1/dripping-birdbath.jpg" alt="Bird sitting on a birdbath with water dripping off it." />
+            <img src="https://i.ibb.co/DQKnM4h/dripping-birdbath.png" alt="Bird sitting on a birdbath with water dripping off it." />
             <p>
                 This is the home page!
             </p>

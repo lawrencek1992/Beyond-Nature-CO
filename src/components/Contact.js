856eda7ef27d90d3b3c35184e18a1627256318a0
Kelly Lawrence <lawrencek1992@gmail.com>
2021-04-30 19:21:59 -0700

    Add links to Fb and Insta icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,8 +10,18 @@ const Contact = () => {
         <Container className="contact-container" fluid>
             <h1>Contact Section</h1>
             <p>Look, a contact page!</p>
-            <FontAwesomeIcon icon={faInstagram} />
-            <FontAwesomeIcon icon={faFacebookSquare} />
+
+
+            {/* Open this in a new tab instead! */}
+            <a href="https://www.instagram.com/beyondnatureco/" className="link-info">
+                <FontAwesomeIcon icon={faInstagram} />
+            </a>
+
+            
+             {/* Open this in a new tab instead! */}
+            <a href="https://www.facebook.com/BeyondNatureCO/" className="link-info">
+                <FontAwesomeIcon icon={faFacebookSquare} />
+            </a>
             <p>Email Address Here!</p>
             <p>Phone Number Here?</p>
             <p>What about an address or a map?</p>

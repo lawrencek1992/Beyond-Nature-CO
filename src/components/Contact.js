d9f4d7fb9272856ae90e2c500168ecaa5a85c783
Kelly Lawrence <lawrencek1992@gmail.com>
2021-04-30 19:10:07 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
@@ -6,14 +7,15 @@ import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
     return (
-        <div>
+        <Container className="contact-container" fluid>
+            <h1>Contact Section</h1>
             <p>Look, a contact page!</p>
             <FontAwesomeIcon icon={faInstagram} />
             <FontAwesomeIcon icon={faFacebookSquare} />
             <p>Email Address Here!</p>
             <p>Phone Number Here?</p>
             <p>What about an address or a map?</p>
-        </div>
+        </Container>
     )
 }
 

f26b51fe74d80881fb5d4275a58aefbda979730f
Kelly Lawrence <lawrencek1992@gmail.com>
2021-04-30 18:44:06 -0700

    Upload Contact.js component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from './components/Header.js';
 import Home from './components/Home.js';
+import Contact from './components/Contact.js';
 
 function App() {
   return (

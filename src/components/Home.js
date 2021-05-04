07ffa6f8a40f0c0599fd04bd064acd7bffc09ecd
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-03 18:22:51 -0700

    Add oxford comma to home page blurb

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
             <h1 className="title">Beyond Nature CO</h1>
             <div className="home-description">
                 <p>
-                    Beyond Nature CO is a small business based out of Erie, CO that makes handmade birdbaths, feeders and garden art from upcycled local materials. Every piece is one of a kind. We pride ourselves on creating sustainable and eco-friendly art. We are proudly a female-owned small business and love supporting other artists in our community. 
+                    Beyond Nature CO is a small business based out of Erie, CO that makes handmade birdbaths, feeders, and garden art from upcycled local materials. Every piece is one of a kind. We pride ourselves on creating sustainable and eco-friendly art. We are proudly a female-owned small business and love supporting other artists in our community. 
                 </p>
                 <p>
                     Check out the highlights section of our site to see some of our best work, and please visit our Instagram or Facebook pages to view the pieces we currently have in stock!

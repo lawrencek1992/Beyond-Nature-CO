9f43913b2f2ba511e48ad6892e36faa925c05f90
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-05 14:48:16 -0700

    Create skeleton for react-infinite-scroll in Highlights.js

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 
+// photo imports here
+
 const Highlights = () => {
     return (
-        <Container className="highlights mt-5" fluid>
-            <p className="highlights-description">
-                Oh, look, a highlights page!
-            </p>
+        // All the 'is-' and 'has-' classes are Bulma CSS classes, and you will probably take them out and replace them with Bootstrap classes. 
+        <Container className="highlights mt-5 hero is-fullheight is-bold is-info" fluid>
+            <div className="hero-body">
+                <div className="container">
+                    <div className="header content">
+                        <h2 className="subtitle is-6">
+                            Code Challenge #16
+                        </h2>
+                        <h1 className="title is-1">
+                            Infinite Scroll Unsplash Code Challenge
+                        </h1>
+                    </div>
+                    {/* IMAGES GO HERE! */}
+                </div>
+            </div>
         </Container>
     );
 }

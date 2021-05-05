0a31468331dfba4ef3d5eb2317827d635f7884d4
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-04 17:47:05 -0700

    Change Carousel.js photos

--- src/components/PhotoCarousel.js
+++ src/components/PhotoCarousel.js
@@ -1,42 +1,50 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import hangingPorcelainFeeders from '../photos/hanging-porcelain-feeders.png';
+import logo from '../photos/Logo.png'
 import blueBowl from '../photos/blue-bowl.png';
-import hangingFeeder from '../photos/hanging-feeder.png';
-import flowersAndBirdseed from '../photos/flowers-and-birdseed.png';
-import crystalBirdBath from '../photos/crystal-birdbath.png';
+import greenBirdbath from '../photos/green-birdbath.png';
+import greenLeaf from '../photos/green-leaf.png';
 
 const PhotoCarousel = () => {
     return (
-        <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 main-image"
-      src={blueBowl}
-      alt="Blue bowl with water hanging on a fence in a meadow"
-    />
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 main-image"
-      src={hangingFeeder}
-      alt="A bird feeder hangs from a tree"
-    />
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 main-image"
-      src={flowersAndBirdseed}
-      alt="A bird feeder hangs on a fence next to pink flowers"
-    />
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 main-image"
-      src={crystalBirdBath}
-      alt="Birdbath made of crystal sparkles in the sunlight"
-    />
-  </Carousel.Item>
-</Carousel>
+        <Carousel class="carousel">
+          <Carousel.Item class="carousel-item">
+            <img
+              className="d-block w-100 main-image"
+              src={logo}
+              alt="Beyond Nature CO logo"
+            />
+          </Carousel.Item>
+          <Carousel.Item class="carousel-item">
+            <img 
+              className="d-block w-100 main-image"
+              src={greenLeaf}
+              alt="A porcelain leaf-shaped birdbath sits in a black basin"
+            />
+          </Carousel.Item>
+          <Carousel.Item class="carousel-item">
+            <img 
+              className="d-block w-100 main-image"
+              src={hangingPorcelainFeeders}
+              alt="Three porcelain bird feeders hang from a fence"
+            />
+          </Carousel.Item>
+          <Carousel.Item class="carousel-item">
+            <img
+              className="d-block w-100 main-image"
+              src={blueBowl}
+              alt="A blue glass birdbath hangs on a fence post in a field"
+            />
+          </Carousel.Item>
+          <Carousel.Item class="carousel-item">
+            <img
+              className="d-block w-100 main-image"
+              src={greenBirdbath}
+              alt="A birdbath made of green porcelain leaves"
+            />
+          </Carousel.Item>
+        </Carousel>
     )
 }
 

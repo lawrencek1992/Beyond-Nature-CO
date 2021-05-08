f8fe5275551fe29b91e4a3c744bae0d8b1365568
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-08 12:33:52 -0700

    Add base Highlights.js styles

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,25 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 
-// photo imports here
-
 const Highlights = () => {
+    // const [photos, setPhotos] = useState(photosArray);
     return (
-        // All the 'is-' and 'has-' classes are Bulma CSS classes, and you will probably take them out and replace them with Bootstrap classes. 
-        <Container className="highlights mt-5 hero is-fullheight is-bold is-info" fluid>
-            <div className="hero-body">
-                <div className="container">
-                    <div className="header content">
-                        <h2 className="subtitle is-6">
-                            Code Challenge #16
-                        </h2>
-                        <h1 className="title is-1">
-                            Infinite Scroll Unsplash Code Challenge
-                        </h1>
-                    </div>
-                    {/* IMAGES GO HERE! */}
-                </div>
-            </div>
+        <Container className="highlights-container" fluid>
+            {/* <Image className="family-photo" src= /> */}
         </Container>
     );
 }

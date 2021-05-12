9d414677cd166d2ec2214d1aade9f5617896f02e
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-11 17:53:52 -0700

    Add message to contact us if user views all photos in Highlights.js

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 // Still using this?
 // import InfiniteScroll from 'react-infinite-scroll-component';
 import Image from 'react-bootstrap/Image';
@@ -31,7 +32,7 @@ function Highlights() {
 
     const photosNextBatch = async (lastIndex) => {
         // You will have to change this to a higher number once you upload all of the photos to firestore! Don't forget!!!!
-        if (lastIndex < 11) {
+        if (lastIndex < 65) {
             const response = db
                 .collection("highlights-photos")
                 .orderBy("index", "asc")
@@ -48,31 +49,7 @@ function Highlights() {
             setLastPhoto(lastPhoto.index);
             return { morePhotos, lastPhoto };
         }
-        // Yo, this if statement isn't firing though....
-        else {
-            return (
-                <div className="text-center">
-                    End of photos!
-                </div>
-            )
-        }
     };
-
-    // Probably getting rid of this.....
-
-    // const fetchMorePhotos = (currentIndex) => {
-    //     setPhotosLoading(true);
-    //     photosNextBatch(currentIndex)
-    //         .then((res) => {
-    //             setLastPhoto(res.lastPhoto.index);
-    //             setPhotos(photos.concat(res.morePhotos));
-    //             setPhotosLoading(false);
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //             setPhotosLoading(false);
-    //         })
-    // }
 
     const fetchMorePhotos = (currentIndex) => {
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
@@ -106,127 +83,14 @@ function Highlights() {
                 })
             }
             </Container>
-            {/* Maybe add conditional stuff down here to render some sort of prompt to contact Kayla and buy her pieces (which also clues in the user that there are no more photos). */}
+            { lastPhoto === 64 && 
             <div className="text-center">
-                {/* {photosLoading 
-                    ? (<p>Loading..</p>) 
-                    : lastPhoto.length > 0 
-                        ? ( */}
-                            
-                            {/* <button onClick={() => photosNextBatch(lastPhoto)}>More Photos</button> */}
-                        
-                        {/* )  */}
-                    {/* : (<span>You are up to date!</span>)
-                } */}
-            </div>
+                <p>
+                    Contact me <Link to="/contact" className="highlights-link">here</Link> to inquire about our current inventory! 
+                </p>
+            </div>}
         </Container>
     )
 }
-// const Highlights = () => {
-//     const [photos, setPhotos] = useState([]);
-//     const [lastKey, setLastKey] = useState('');
-//     const [nextPhotos_loading, setNextPhotosLoading] = useState(false);
-
-//     const db = firebase.firestore();
-
-//     const photosFirstBatch = async () => {
-//         try {
-//             const data = await db
-//                 .collection('highlights-photos')
-//                 .orderBy('createdAt', 'desc')
-//                 .limit(5)
-//                 .get();
-            
-//             let photos = [];
-//             let lastKey = '';
-//             data.docs.forEach((doc) => {
-//                 photos.push({
-//                     photoURL: doc.data().url,
-//                 })
-//                 lastKey = doc.data().createdAt;
-//             });
-//             return { photos, lastKey };
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     };
-
-//    const photosNextBatch = async (key) => {
-//        try {
-//            const data = await db
-//                 .collection('highlights-photos')
-//                 .orderedBy('createdAt', 'desc')
-//                 .startAfter(key)
-//                 .limit(5)
-//                 .get();
-
-//             let photos = [];
-//             let lastKey = '';
-//             data.forEach((doc) => {
-//                 photos.push({
-//                     photoURL: doc.data().url,
-//                 })
-//                 lastKey = doc.data().createdAt;
-//             });
-//             return { photos, lastKey };
-//        } catch (error) {
-//            console.log(error);
-//        }
-//    };
-
-//    useEffect(()=> {
-//        photosFirstBatch()
-//         .then((res) => {
-//             setPhotos(res.photos);
-//             setLastKey(res.lastKey);
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         });
-//     });
-
-//    const fetchMorePhotos = (key) => {
-//        if (key.length > 0) {
-//            setNextPhotosLoading(true);
-//            photosNextBatch(key)
-//             .then((res) => {
-//                 setLastKey(res.lastKey);
-//                 // add new photos to old photos
-//                 setPhotos(photos.concat(res.photos));
-//                 setNextPhotosLoading(false);
-//             })
-//             .catch((error) => {
-//                 console.log(error);
-//                 setNextPhotosLoading(false);
-//             });
-//        }
-//    };
-
-//    const allPhotos = (
-//        <div>
-//            {photos.map((photo) => {
-//                return (
-//                    <Image src={photos.photoURL} alt="Photo of garden decor made by Beyond Nature CO" />
-//                );
-//            })}
-//        </div>
-//    );
-
-//     return (
-//         <Container className="highlights-container mt-5 pt-3" fluid>
-//             <h1 id="highlights-title">Highlights</h1>
-//             <div>{allPhotos}</div>
-//             <div>
-//                 {nextPhotos_loading ? (
-//                     <p>Loading..</p>
-//                 ) : lastKey.length > 0 ? (
-//                     <button onClick={() => fetchMorePhotos(lastKey)}>More Photos</button>
-//                 ) : (
-//                     <span>No more photos!</span>
-//                 )}
-//             </div>
-//         </Container>
-//     );
-// }
 
 export default Highlights;

190eb777547321f44e1fb22c8d2aed92e1b2cc50
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-12 00:21:12 -0600

    Add onLogout and onLogin functions to App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,66 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import firebase from "./firebase";
 
 import Header from './components/Header.js';
 import Home from './components/Home.js';
 import Contact from './components/Contact.js';
 import Highlights from './components/Highlights.js';
+import Login from './components/Login/js';
 
 const App = () => {
+
+  const onLogin = (email, password) => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+  // Do you want to do anything with state to display a user thing like you did with you blog?
+      // .then((response) => {
+
+      // Do you want a login message? Write one....
+    // setFlashMessage(`login`);
+
+      //   if (response.user["email"].includes("demo")) {
+      //     setUser({
+      //       email: response.user["email"],
+      //       isAuthenticated: true,
+      //       username: "Demo Account",
+      //     });
+      //   } else {
+      //     setUser({
+      //       email: response.user["email"],
+      //       isAuthenticated: true,
+      //       username: "Kelly Lawrence",
+      //     })
+      //   }
+      // })
+      .catch((error) => console.error(error));
+      return (
+        <Redirect to="/" />
+      )
+    };
+  
+  const onLogout = () => {
+    firebase
+      .auth()
+      .signOut()
+  // Do you want to do anything with this? ^See above
+      // .then(() => {
+      //   setUser({ isAuthenticated: false});
+      // })
+      .catch((error) => console.error(error));
+  // You need to actually write a flash message....
+    // setFlashMessage(`logout`);
+    return (
+      <Redirect to="/" />
+    )
+  };
+
   return (
     <Router>
       <div className="App">
@@ -32,6 +82,11 @@ const App = () => {
             exact
             path="/highlights"
             render={() => <Highlights />}
+          />
+          <Route
+            exact
+            path="/login"
+            render={() => <Login />}
           />
         </Switch>
       </div>

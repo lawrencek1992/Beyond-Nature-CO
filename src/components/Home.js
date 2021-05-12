bb52eb5d882f84629b29ba5a0bd6091eedaf18e5
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-12 10:21:15 -0600

    Change margins on Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
                         <p>
                             Beyond Nature CO is a small business based out of Erie, CO that makes handmade birdbaths, feeders, and garden art from upcycled local materials. Every piece is one of a kind. We pride ourselves on creating sustainable and eco-friendly art. We are proudly a female-owned small business and love supporting other artists in our community. 
                         </p>
-                        <p className="mb-5">
+                        <p className="mb-2">
                             Check out the highlights section of our site to see some of our best work, and please visit our Instagram or Facebook pages to view the pieces we currently have in stock!
                         </p>
                     </Container>

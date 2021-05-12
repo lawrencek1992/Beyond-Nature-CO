3adb8ff5a6751ccf0d9d5fdb26e091dd804ad6f7
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-12 00:20:58 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,19 +8,19 @@ const Home = () => {
     return (
         <Container className="home-container pb-5 text-center" fluid >
             <PhotoCarousel />
-            <h1 className="about mt-5 mb-0">About</h1>
             <Container className="about-container">
-                <Container className="home-description mt-0 pt-0 col-s-7 text-center">
-                    <p>
-                        Beyond Nature CO is a small business based out of Erie, CO that makes handmade birdbaths, feeders, and garden art from upcycled local materials. Every piece is one of a kind. We pride ourselves on creating sustainable and eco-friendly art. We are proudly a female-owned small business and love supporting other artists in our community. 
-                    </p>
-                    <p className="mb-3">
-                        Check out the highlights section of our site to see some of our best work, and please visit our Instagram or Facebook pages to view the pieces we currently have in stock!
-                    </p>
-                </Container>
-                <Container>
-                    <Image className="headshot" src={headshot} alt="Kayle Eggemeyer, owner of Beyond Nature CO." fluid />
-                </Container>
+                <h1 className="about mt-5 mb-0">About</h1>
+                    <Container className="home-description mt-0 pt-0 col-s-7 text-center">
+                        <p>
+                            Beyond Nature CO is a small business based out of Erie, CO that makes handmade birdbaths, feeders, and garden art from upcycled local materials. Every piece is one of a kind. We pride ourselves on creating sustainable and eco-friendly art. We are proudly a female-owned small business and love supporting other artists in our community. 
+                        </p>
+                        <p className="mb-5">
+                            Check out the highlights section of our site to see some of our best work, and please visit our Instagram or Facebook pages to view the pieces we currently have in stock!
+                        </p>
+                    </Container>
+                    <Container>
+                        <Image className="headshot" src={headshot} alt="Kayle Eggemeyer, owner of Beyond Nature CO." fluid />
+                    </Container>
             </Container>
         </Container>
     )

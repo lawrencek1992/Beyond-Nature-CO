2190aec192bd960db871ac3c50c65fe8fc04b869
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-14 20:08:40 -0600

    Update InventoryForm.js

--- src/components/InventoryForm.js
+++ src/components/InventoryForm.js
@@ -4,13 +4,12 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const InventoryForm = () => {
+    
+
     return (
         <Container className="inventory-form-container pt-5 mt-5" fluid>
             <h1 className="text-center title mb-4 mt-3">Add Inventory</h1>
-            <Form className="inventory-form p-4">
-                <Form.Group controlId="photoUpload">
-                    <Form.File id="image" label="Image upload" className="form-label" />
-                </Form.Group>
+            <Form className="inventory-form p-4 mb-5">
                 <Form.Group controlId="caption">
                     <Form.Label className="form-label">Caption</Form.Label>
                     <Form.Control type="textarea" placeholder="Item description"/>
@@ -19,7 +18,12 @@ const InventoryForm = () => {
                     <Form.Label className="form-label">Price</Form.Label>
                     <Form.Control type="number" placeholder="149.99" />
                 </Form.Group>
-                <Button className="btn upload-button mb-2">Upload</Button>
+                <Form.Group controlId="photoUpload">
+                    <Form.File id="image" label="Image upload" className="form-label" />
+                </Form.Group>
+                <Form.Group clas>
+                    <Button className="btn upload-button mb-0 mt-2" type="submit">Upload</Button>
+                </Form.Group>
             </Form>
         </Container>
     )

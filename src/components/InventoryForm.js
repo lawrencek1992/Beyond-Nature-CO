33891d04d66d53d543644eda7bd1a2ea421a3d1f
Kelly Lawrence <lawrencek1992@gmail.com>
2021-05-14 19:51:06 -0600

    Add bootstrap form to InventoryForm.js

--- src/components/InventoryForm.js
+++ src/components/InventoryForm.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const InventoryForm = () => {
     return (
         <Container className="inventory-form-container pt-5 mt-5" fluid>
-            <h1>Manage inventory</h1>
-            <Form className="inventory-form">
+            <h1 className="text-center title mb-4 mt-4">Add Inventory</h1>
+            <Form className="inventory-form p-4">
                 <Form.Group controlId="photoUpload">
-                <Form.File id="image" label="Image upload" />
+                    <Form.File id="image" label="Image upload" className="form-label" />
                 </Form.Group>
-                <Form.Label>Caption</Form.Label>
-                <Form.Control type="textarea"/>
+                <Form.Group controlId="caption">
+                    <Form.Label className="form-label">Caption</Form.Label>
+                    <Form.Control type="textarea" placeholder="Item description"/>
+                </Form.Group>
+                <Form.Group controlId="price">
+                    <Form.Label className="form-label">Price</Form.Label>
+                    <Form.Control type="number" placeholder="149.99" />
+                </Form.Group>
+                <Button className="btn upload-button mb-2">Upload</Button>
             </Form>
         </Container>
     )

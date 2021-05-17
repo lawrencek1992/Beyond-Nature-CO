0b2762509ededb54cb270d68c5cc85912a7f3b70
Kelly <lawrencek1992@gmail.com>
2021-05-17 14:41:00 -0600

    Import new carousel photos in PhotoCarousel.js

--- src/components/PhotoCarousel.js
+++ src/components/PhotoCarousel.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import hangingPorcelainFeeders from '../photos/hanging-porcelain-feeders.png';
 import logo from '../photos/Logo.png';
+
+// First round of potenttial
+import hangingPorcelainFeeders from '../photos/hanging-porcelain-feeders.png';
 import greenBirdbath from '../photos/green-birdbath.png';
 import greenLeaf from '../photos/green-leaf.png';
+
+// Try two with photos:
+import hangingGlassFeeders from '../photos/hanging-glass-feeders.png';
+import owlBirdbath from '../photos/owl-birdbath.png';
+import greenCrystalBirdbath from '../photos/green-cystal-birdbath.png';
 
 const PhotoCarousel = () => {
     return (
@@ -18,21 +25,21 @@ const PhotoCarousel = () => {
           <Carousel.Item className="carousel-item" interval={3000}>
             <img 
               className="d-block w-100 main-image"
-              src={hangingPorcelainFeeders}
+              src={greenBirdbath}
               alt="Three porcelain bird feeders hang from a fence"
             />
           </Carousel.Item>
           <Carousel.Item className="carousel-item" interval={3000}>
             <img 
               className="d-block w-100 main-image"
-              src={greenLeaf}
+              src={hangingGlassFeeders}
               alt="A porcelain leaf-shaped birdbath sits in a black basin"
             />
           </Carousel.Item>
           <Carousel.Item className="carousel-item" interval={3000}>
             <img
               className="d-block w-100 main-image"
-              src={greenBirdbath}
+              src={owlBirdbath}
               alt="A birdbath made of green porcelain leaves"
             />
           </Carousel.Item>

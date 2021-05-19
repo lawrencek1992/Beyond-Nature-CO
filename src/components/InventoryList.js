0a67878aa0c5031dd5fb5758ff6f6c6faab6a4df
Kelly <lawrencek1992@gmail.com>
2021-05-19 15:20:31 -0600

    Update InventoryList.js styling

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 const InventoryList = () => {
     return (
-        <Container className="inventory-list-container text-center" fluid>
+        <Container className="inventory-list-container text-center pl-4 pr-4" fluid>
             <h1 className="title pt-5 mt-5 mb-3">
                 Current Inventory
             </h1>
             <Row>
-                <Col s={5}>
-                   <Card className="inventory-card mb-3">
-                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" />
+                <Container className="col-md-3 col-sm-4 mb-3">
+                    <Card className="inventory-card mb-3">
+                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" className="card-image" fluid />
                         <Card.Body>
                             <Card.Title className="description">
                                 Item Description
@@ -23,11 +22,11 @@ const InventoryList = () => {
                                 $100
                             </Card.Text>
                         </Card.Body>
-                   </Card> 
-                </Col>
-                <Col s={5}>
-                   <Card className="inventory-card mb-3">
-                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" />
+                    </Card>
+                </Container>
+                <Container className="col-md-3 col-sm-4 mb-3">
+                    <Card className="inventory-card mb-3">
+                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" className="card-image" fluid />
                         <Card.Body>
                             <Card.Title className="description">
                                 Item Description
@@ -36,11 +35,11 @@ const InventoryList = () => {
                                 $100
                             </Card.Text>
                         </Card.Body>
-                   </Card> 
-                </Col>
-                <Col s={5}>
-                   <Card className="inventory-card mb-3">
-                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" />
+                    </Card>
+                </Container>
+                <Container className="col-md-3 col-sm-4 mb-3">
+                    <Card className="inventory-card mb-3">
+                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" className="card-image" fluid />
                         <Card.Body>
                             <Card.Title className="description">
                                 Item Description
@@ -49,11 +48,11 @@ const InventoryList = () => {
                                 $100
                             </Card.Text>
                         </Card.Body>
-                   </Card> 
-                </Col>
-                <Col s={5}>
-                   <Card className="inventory-card mb-3">
-                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" />
+                    </Card>
+                </Container>
+                <Container className="col-md-3 col-sm-4 mb-3">
+                    <Card className="inventory-card mb-3">
+                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" className="card-image" fluid />
                         <Card.Body>
                             <Card.Title className="description">
                                 Item Description
@@ -62,21 +61,8 @@ const InventoryList = () => {
                                 $100
                             </Card.Text>
                         </Card.Body>
-                   </Card> 
-                </Col>
-                <Col s={5}>
-                   <Card className="inventory-card mb-3">
-                        <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/beyond-nature-co-95ab7.appspot.com/o/highlights-photos%2Fphoto-0.jpg?alt=media&token=3be4a11a-952b-4cb6-9e78-7ba12cb9d7c3" />
-                        <Card.Body>
-                            <Card.Title className="description">
-                                Item Description
-                            </Card.Title>
-                            <Card.Text className="price">
-                                $100
-                            </Card.Text>
-                        </Card.Body>
-                   </Card> 
-                </Col>
+                    </Card>
+                </Container>
             </Row>
         </Container>
     )
